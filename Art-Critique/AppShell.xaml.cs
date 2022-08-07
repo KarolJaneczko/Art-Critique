6865c9cfaab06487f53660bd66dfadc394e77baf
Karol Janeczko <karol.janeczko@outlook.com>
2022-08-07 13:42:39 +0200

    Pierwszy page

--- Art-Critique/AppShell.xaml.cs
+++ Art-Critique/AppShell.xaml.cs
@@ -4,16 +4,16 @@ using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
 
-namespace Art_Critique
-{
-    public partial class AppShell : Xamarin.Forms.Shell
-    {
-        public AppShell()
-        {
+namespace Art_Critique {
+    public partial class AppShell : Xamarin.Forms.Shell {
+        public AppShell() {
             InitializeComponent();
             Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            RegisterRoutes();
         }
-
+        public void RegisterRoutes() {
+            Routing.RegisterRoute(nameof(HomePage), typeof(HomePage));
+        }
     }
 }
---
+++ Art-Critique/Views/HomePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Art_Critique.Views {
+    public partial class HomePage : ContentPage {
+        public HomePage() {
+            InitializeComponent();
+        }
+    }
+}
