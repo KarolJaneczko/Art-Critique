8f447d3448a9d6b96f37a2d890142c6ac55fd05f
Karol Janeczko <karol.janeczko@outlook.com>
2022-10-16 23:08:42 +0200

    porządki

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,18 +1,14 @@
 ﻿namespace Art_Critique;
 
-public static class MauiProgram
-{
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+public static class MauiProgram {
+    public static MauiApp CreateMauiApp() {
+        var builder = MauiApp.CreateBuilder();
 
-		return builder.Build();
-	}
+        builder.UseMauiApp<App>().ConfigureFonts(fonts => {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+        });
+
+        return builder.Build();
+    }
 }
