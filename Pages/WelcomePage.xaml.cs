bd38a69ced6f5c57c56ae7e211659f97113d79b1
Karol Janeczko <karol.janeczko@outlook.com>
2022-11-12 00:47:52 +0100

    poprawki w ekranie powitalnym

--- Pages/WelcomePage.xaml.cs
+++ Pages/WelcomePage.xaml.cs
@@ -3,5 +3,7 @@
 public partial class WelcomePage : ContentPage {
     public WelcomePage() {
         InitializeComponent();
+        //this.BackgroundImageSource = "welcomepage_background.png";
+        
     }
 }
