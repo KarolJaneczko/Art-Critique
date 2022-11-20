77fcde5e2225883c2b7c19a6deec9866e344f81e
Karol Janeczko <karol.janeczko@outlook.com>
2022-11-20 19:40:16 +0100

    Start pracy nad apką

--- Pages/WelcomePage.xaml.cs
+++ Pages/WelcomePage.xaml.cs
@@ -1,7 +1,14 @@
-﻿namespace Art_Critique;
+﻿using Art_Critique.Core.Logic;
+
+namespace Art_Critique;
 
 public partial class WelcomePage : ContentPage {
     public WelcomePage() {
         InitializeComponent();
+        OnPageCreating();
+    }
+    
+    private void OnPageCreating() {
+        WelcomeLabel.Margin = new Thickness(DeviceProperties.GetHeightPercent(1), DeviceProperties.GetHeightPercent(5), 0, 0);
     }
 }
