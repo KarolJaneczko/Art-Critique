a4a457c4f3474ff585035b1d82632bb75861f3d7
Karol Janeczko <karol.janeczko@outlook.com>
2023-07-27 20:51:18 +0200

    poprawki w profilu

---
+++ Art-Critique-App/Pages/ProfilePages/GalleryPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Art_Critique.Models.API.Artwork;
+using Art_Critique.Pages.ProfilePages;
+using Art_Critique.Services.Interfaces;
+using Art_Critique.Utils.Helpers;
+using Newtonsoft.Json;
+
+namespace Art_Critique {
+    [QueryProperty(nameof(Login), nameof(Login))]
+    public partial class GalleryPage : ContentPage {
+        #region Services
+        private readonly IHttpService HttpService;
+        #endregion
+
+        #region Properties
+        private string login;
+        public string Login { get => login; set { login = value; OnPropertyChanged(nameof(Login)); } }
+        #endregion
+
+        #region Constructor
+        public GalleryPage(IHttpService httpService) {
+            InitializeComponent();
+            HttpService = httpService;
+            InitializeValues();
+        }
+        #endregion
+
+        #region Methods
+        private void InitializeValues() {
+            Routing.RegisterRoute(nameof(EditProfilePage), typeof(EditProfilePage));
+        }
+
+        protected override async void OnNavigatedTo(NavigatedToEventArgs args) {
+            base.OnNavigatedTo(args);
+            var task = new Func<Task>(async () => {
+                // Loading user's artworks.
+                var artworks = await HttpService.SendApiRequest(HttpMethod.Get, $"{Dictionary.GetUserArtworks}?login={Login}");
+                var thumbnails = JsonConvert.DeserializeObject<List<ApiCustomPainting>>(artworks.Data.ToString());
+                BindingContext = new GalleryPageViewModel(thumbnails);
+            });
+            // Run task with try/catch.
+            await MethodHelper.RunWithTryCatch(task);
+        }
+        #endregion
+    }
+}
