93e39e02374c220b5765195d60e8eef098fe42bc
Karol Janeczko <karol.janeczko@outlook.com>
2023-08-03 21:19:44 +0200

    przeniesienie welcomepage do startpages

---
+++ Art-Critique-App/Pages/StartPages/WelcomePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace Art_Critique {
+    public partial class WelcomePage : ContentPage {
+        #region Constructor
+        public WelcomePage() {
+            InitializeComponent();
+            InitializeValues();
+        }
+        #endregion
+
+        #region Methods
+        private void InitializeValues() {
+            Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
+            Routing.RegisterRoute(nameof(RegisterPage), typeof(RegisterPage));
+        }
+
+        private async void GoToLogin(object sender, EventArgs args) {
+            await Shell.Current.GoToAsync(nameof(LoginPage));
+        }
+
+        private async void GoToRegister(object sender, EventArgs args) {
+            await Shell.Current.GoToAsync(nameof(RegisterPage));
+        }
+
+        protected override bool OnBackButtonPressed() {
+            return true;
+        }
+        #endregion
+    }
+}
